8e6aa951deffef9738d49b8649ebc1b24f698a7e
lizergin88 <oleg.minda@gmail.com>
2017-10-30 00:19:44 +0700

    Sceleton Crimemap

--- crimemap.py
+++ crimemap.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from dbhelper import DBHelper
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+DB = DBHelper()
 
 
 @app.route("/")
-def index():
-    return "Hello, World!"
+def home():
+    try:
+        data = DB.get_all_inputs()
+    except Exception as e:
+        print(e)
+        data = None
+    return render_template("home.html", data=data)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    try:
+        data = request.form.get("userinput")
+        DB.add_input(data)
+    except Exception as e:
+        print(e)
+    return home()
+
+
+@app.route("/clear")
+def clear():
+    try:
+        DB.clear_all()
+    except Exception as e:
+        print(e)
+    return home()
 
 
 if __name__ == '__main__':
---
+++ dbhelper.py
@@ -0,0 +1,44 @@
+import pymysql
+import dbconfig
+
+
+class DBHelper:
+
+    def connect(self, database="crimemap"):
+       return pymysql.connect(host='localhost',
+                 user=dbconfig.db_user,
+                 passwd=dbconfig.db_password,
+                 db=database)
+
+    def get_all_inputs(self):
+        connection = self.connect()
+        try:
+         query = "SELECT description FROM crimes;"
+         with connection.cursor() as cursor:
+           cursor.execute(query)
+         return cursor.fetchall()
+        finally:
+         connection.close()
+
+    def add_input(self, data):
+        connection = self.connect()
+        try:
+         # The following introduces a deliberate security flaw.
+         # See section on SQL injection below
+         query = "INSERT INTO crimes (description) VALUES
+         ('{}');".format(data)
+         with connection.cursor() as cursor:
+           cursor.execute(query)
+           connection.commit()
+        finally:
+         connection.close()
+
+    def clear_all(self):
+       connection = self.connect()
+       try:
+         query = "DELETE FROM crimes;"
+         with connection.cursor() as cursor:
+           cursor.execute(query)
+           connection.commit()
+       finally:
+        connection.close()
